f6e957e2e8eee2d8e2d3486b6a8939b4d75ce3ac
Alex <alex@monney.me>
2018-01-03 18:23:22 +0000

    Add routes_found property

--- FlightLog.js
+++ FlightLog.js
@@ -1,22 +1,13 @@
 "use strict";
 
 module.exports = class FlightLog {
-  constructor(csv = ''){
-    this.flights = {
-      "a" : [
-        { code : "b", dist: 200, rowno: 1 },
-        { code : "c", dist: 300, rowno: 2 },
-      ],
-      "b": [
-        { code : "b", dist: 0, rowno: 3 },
-        { code : "c", dist: 100, rowno: 4 },
-        { code : "a", dist: 200, rowno: 5 },
-        { code : "a", dist: 200, rowno: 6 },
-      ],
-      "c": [
-        { code : "a", dist: 300, rowno: 7 },
-      ]
+  constructor(csv, object = {}){
+    if (csv){
+
+    } else {
+      this.flights = object
     }
+    this.routes_found = [];
   }
 
   dump(){
@@ -30,24 +21,28 @@ module.exports = class FlightLog {
 
   filter_circular_flights(){
     for (var start in this.flights){
-      this.flights[start] = this.flights[start].filter(end => start != end.code )
+      this.flights[start] = this.flights[start].filter(end => {
+        if (start == end.code) this.routes_found.push([start, end.code]);
+        return start != end.code;
+      })
     }
   }
 
   filter_out_and_return(){
     for (var start in this.flights){
-      this.flights[start] = this.flights[start].filter(end => this.dead_end(start, end.code))
+      this.flights[start] = this.flights[start].filter(end => this._dead_end(start, end.code))
     }
   }
 
-  dead_end(home, loc, flights = this.flights){
-    if (!flights[loc]) return true; // no flights out of here to test
+  _dead_end(home, loc){
+    if (!this.flights[loc]) return true; // no flights out of here to test
 
     // loop through other places we went from destination airport
-    let returnIndex = flights[loc].findIndex(el => el.code == home)
+    let returnIndex = this.flights[loc].findIndex(el => el.code == home)
     if ( returnIndex != -1){
-      // Found a route home. Remove from flight data and return
-      flights[loc].splice(returnIndex,1);
+      // Found a route home. Add to routes_found, remove from flight data and return
+      this.routes_found.push([home, loc, home]);
+      this.flights[loc].splice(returnIndex,1);
       return false; 
     }
     
--- test/01-FlightLog.js
+++ test/01-FlightLog.js
@@ -3,14 +3,89 @@
 let should  = require('should')
 let FL      = require('../Flightlog');
 
+let test_data = {
+  "a" : [
+    { code : "a", dist: 0, rowno: 1 },
+    { code : "b", dist: 300, rowno: 2 },
+    { code : "c", dist: 500, rowno: 3 },
+  ],
+  "b": [
+    { code : "a", dist: 300, rowno: 4 },
+    { code : "a", dist: 300, rowno: 5 },
+    { code : "b", dist: 0, rowno: 6 },
+    { code : "c", dist: 400, rowno: 7 },
+  ],
+  "c": [
+    { code : "a", dist: 500, rowno: 8 },
+  ]
+}
+
 describe('Test constructor', () => {
-  
+  it('should be able to build an object with test object', done => {
+    let fl = new FL(undefined, test_data);
+    fl.flights.should.be.an.Object();
+    fl.flights.a.should.be.an.Array();
+    done();
+  })
 });
 
 describe('Test object methods', () => {
-  let fl = new FL();
-  it('Should have a dump method', done => {
+
+  it('should have a dump method', done => {
+    let fl = new FL(undefined, test_data);
     fl.dump.should.be.a.Function();
     done();
   })
+
+  it('should be able to filter flights starting and ending at same place', done => {
+    let fl = new FL(undefined, test_data)
+      , b_dests = fl.flights.b.length
+    ;
+
+    fl.flights.b[2].code.should.equal('b');
+    fl.filter_circular_flights();
+    fl.flights.b.length.should.equal(b_dests - 1);
+    fl.flights.b[2].code.should.not.equal('b');
+    fl.routes_found.should.deepEqual([
+      ['a','a'],
+      ['b','b']
+    ])
+    done();
+  })
+
+  it('should be able to pair and filter simple out and return flights', done => {
+    let fl = new FL(undefined, test_data)
+      , a_dests = fl.flights.a.length
+      , b_dests = fl.flights.b.length
+      , c_dests = fl.flights.c.length
+    ;
+
+    fl.filter_out_and_return();
+    fl.flights.a.length.should.equal(a_dests - 2);
+    fl.flights.b.length.should.equal(b_dests - 1);
+    fl.flights.c.length.should.equal(c_dests - 1);
+    fl.routes_found.should.deepEqual([
+      ['a','b','a'],
+      ['a','c','a']
+    ])
+    done();
+  });
+
+  // it('should be able to preferentially filter longest routes', done => {
+  //   let fl = new FL(undefined, test_data)
+  //     , a_dests = fl.flights.a.length
+  //     , b_dests = fl.flights.b.length
+  //     , c_dests = fl.flights.c.length
+  //   ;
+
+  //   fl.filter_long_routes();
+  //   fl.flights.a.length.should.equal(a_dests - 1);
+  //   fl.flights.b.length.should.equal(b_dests - 1);
+  //   fl.flights.c.length.should.equal(c_dests - 1);
+  //   fl.routes_found.should.deepEqual([
+  //     ['a','b','c']
+  //   ])
+  //   done();
+  // });
+
 });
